4f24a7f9a4ae721944a4f6fff83c89c95a4291fd
Adrian Wong <adrianwjw@gmail.com>
2017-06-28 11:06:12 +1000

    refactor: Explicit import of Prelude for Ch07

--- ch07_solutions.hs
+++ ch07_solutions.hs
@@ -1,5 +1,6 @@
 module ProgrammingInHaskell_Ch07 where
 
+import Prelude hiding (all, any, curry, dropWhile, filter, iterate, map, takeWhile, uncurry)
 import Data.Char
 
 type Bit = Int
@@ -11,35 +12,35 @@ listComp f p xs = map f (filter p xs)
 -- Q2a
 {- Question appears to be incorrect. A predicate of type (a -> Bool) cannot
    act on a list that is of type [Bool] -}
-all' :: (a -> Bool) -> [a] -> Bool
-all' p = and . map p
+all :: (a -> Bool) -> [a] -> Bool
+all p = and . map p
 
 -- Q2b
 {- Question appears to be incorrect. A predicate of type (a -> Bool) cannot
    act on a list that is of type [Bool] -}
-any' :: (a -> Bool) -> [a] -> Bool
-any' p = or . map p
+any :: (a -> Bool) -> [a] -> Bool
+any p = or . map p
 
 -- Q2c
-takeWhile' :: (a -> Bool) -> [a] -> [a]
-takeWhile' p [] = []
-takeWhile' p (x:xs)
-    | p x       = x : takeWhile' p xs
+takeWhile :: (a -> Bool) -> [a] -> [a]
+takeWhile p [] = []
+takeWhile p (x:xs)
+    | p x       = x : takeWhile p xs
     | otherwise = []
 
 -- Q2d
-dropWhile' :: (a -> Bool) -> [a] -> [a]
-dropWhile' p [] = []
-dropWhile' p (x:xs)
-    | p x       = dropWhile' p xs
+dropWhile :: (a -> Bool) -> [a] -> [a]
+dropWhile p [] = []
+dropWhile p (x:xs)
+    | p x       = dropWhile p xs
     | otherwise = x:xs
 
 -- Q3
-map' :: (a -> b) -> [a] -> [b]
-map' f = foldr (\x xs -> f x : xs) []
+map :: (a -> b) -> [a] -> [b]
+map f = foldr (\x xs -> f x : xs) []
 
-filter' :: (a -> Bool) -> [a] -> [a]
-filter' p = foldr (\x xs -> if p x then x : xs else xs) []
+filter :: (a -> Bool) -> [a] -> [a]
+filter p = foldr (\x xs -> if p x then x : xs else xs) []
 
 -- Q4
 {- 1000*2 + 100*3 + 10*4 + 1*5
@@ -52,11 +53,11 @@ dec2int :: [Int] -> Int
 dec2int = foldl (\x y -> 10*x + y) 0
 
 -- Q5
-curry' :: ((a,b) -> c) -> (a -> b -> c)
-curry' f = \x y -> f (x,y)
+curry :: ((a,b) -> c) -> (a -> b -> c)
+curry f = \x y -> f (x,y)
 
-uncurry' :: (a -> b -> c) -> ((a,b) -> c)
-uncurry' f = \(x,y) -> f x y
+uncurry :: (a -> b -> c) -> ((a,b) -> c)
+uncurry f = \(x,y) -> f x y
 
 -- Q6
 unfold :: (a -> Bool) -> (a -> b) -> (a -> a) -> a -> [b]
@@ -70,11 +71,11 @@ int2bin = unfold (== 0) (`mod` 2) (`div` 2)
 chop8 :: [Bit] -> [[Bit]]
 chop8 = unfold (null) (take 8) (drop 8)
 
-map'' :: (a -> b) -> [a] -> [b]
-map'' f = unfold (null) (f . head) tail
+map' :: (a -> b) -> [a] -> [b]
+map' f = unfold (null) (f . head) tail
 
-iterate' :: (a -> a) -> a -> [a]
-iterate' f = unfold (const False) id f
+iterate :: (a -> a) -> a -> [a]
+iterate f = unfold (const False) id f
 
 -- Q7
 bin2int :: [Bit] -> Int
