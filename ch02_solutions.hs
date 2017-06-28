441e4d982364dc702fdbc248af2bdb47593db6f8
Adrian Wong <adrianwjw@gmail.com>
2017-06-28 10:11:25 +1000

    refactor: Explicit import of Prelude for Ch02

--- ch02_solutions.hs
+++ ch02_solutions.hs
@@ -1,17 +1,19 @@
 module ProgrammingInHaskell_Ch02 where
 
+import Prelude hiding (init)
+
 -- Q5
+init [] = []
+init xs = take (length xs - 1) xs
+
 init' [] = []
-init' xs = take (length xs - 1) xs
+init' xs = reverse (tail (reverse xs))
 
-init'' [] = []
-init'' xs = reverse (tail (reverse xs))
-
-init''' []     = []
-init''' [_]    = []
-init''' (x:xs) = x : init xs
+init'' []     = []
+init'' [_]    = []
+init'' (x:xs) = x : init xs
 
 dropLast 0 xs     = []
 dropLast 1 (x:xs) = []
 dropLast n (x:xs) = x : dropLast (n-1) xs
-init'''' xs = dropLast (length xs) xs
+init''' xs = dropLast (length xs) xs
