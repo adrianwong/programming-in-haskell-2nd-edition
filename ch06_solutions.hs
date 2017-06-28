cae4c9c40310e714857196c5c3c07e0903ea275a
Adrian Wong <adrianwjw@gmail.com>
2017-06-28 10:27:25 +1000

    refactor: Explicit import of Prelude for Ch06

--- ch06_solutions.hs
+++ ch06_solutions.hs
@@ -1,31 +1,33 @@
 module ProgrammingInHaskell_Ch06 where
 
+import Prelude hiding (and, concat, elem, last, replicate, sum, take, (!!))
+
 -- Q6a
-and' :: [Bool] -> Bool
-and' []     = True
-and' (x:xs) = x && and' xs
+and :: [Bool] -> Bool
+and []     = True
+and (x:xs) = x && and xs
 
 -- Q6b
-concat' :: [[a]] -> [a]
-concat' []       = []
-concat' (xs:xss) = xs ++ concat' xss
+concat :: [[a]] -> [a]
+concat []       = []
+concat (xs:xss) = xs ++ concat xss
 
 -- Q6c
-replicate' :: Int -> a -> [a]
-replicate' 0 _ = []
-replicate' n x = x : replicate (n-1) x
+replicate :: Int -> a -> [a]
+replicate 0 _ = []
+replicate n x = x : replicate (n-1) x
 
 -- Q6d
-bangbang :: [a] -> Int -> a
-bangbang (x:xs) 1 = x
-bangbang (x:xs) n = bangbang xs (n-1)
+(!!) :: [a] -> Int -> a
+(!!) (x:xs) 1 = x
+(!!) (x:xs) n = (!!) xs (n-1)
 
 -- Q6e
-elem' :: Eq a => a -> [a] -> Bool
-elem' e [] = False
-elem' e (x:xs)
+elem :: Eq a => a -> [a] -> Bool
+elem e [] = False
+elem e (x:xs)
     | e == x    = True
-    | otherwise = elem' e xs
+    | otherwise = elem e xs
 
 -- Q7
 merge :: Ord a => [a] -> [a] -> [a]
@@ -47,16 +49,16 @@ msort xs  = merge (msort ys) (msort zs)
             where (ys,zs) = halve xs
 
 -- Q9a
-sum' :: Num a => [a] -> a
-sum' []     = 0
-sum' (x:xs) = x + sum' xs
+sum :: Num a => [a] -> a
+sum []     = 0
+sum (x:xs) = x + sum xs
 
 -- Q9b
-take' :: Int -> [a] -> [a]
-take' 0 xs     = []
-take' n (x:xs) = x : take' (n-1) xs
+take :: Int -> [a] -> [a]
+take 0 xs     = []
+take n (x:xs) = x : take (n-1) xs
 
 -- Q9c
-last' :: [a] -> a
-last' (x:[]) = x
-last' (x:xs) = last' xs
+last :: [a] -> a
+last (x:[]) = x
+last (x:xs) = last xs
